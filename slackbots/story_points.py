a469f262a5999b049ebb1d503b4006ddbfe39777
Bryan Burlingame <bmburlingame@outlook.com>
2018-05-17 10:07:09 -0500

    fixed profile

--- slackbots/story_points.py
+++ slackbots/story_points.py
@@ -17,30 +17,4 @@ def StoryPoints():
         abort(400)
 
 
-    return jsonify({
-        # Uncomment the line below for the response to be visible to everyone
-        # 'response_type': 'in_channel',
-        'text': 'More fleshed out response to the slash command',
-        'attachments': [
-            {
-                'fallback': 'Required plain-text summary of the attachment.',
-                'color': '#36a64f',
-                'pretext': 'Optional text above the attachment block',
-                'author_name': 'Bobby Tables',
-                'author_link': 'http://flickr.com/bobby/',
-                'author_icon': 'http://flickr.com/icons/bobby.jpg',
-                'title': 'Slack API Documentation',
-                'title_link': 'https://api.slack.com/',
-                'text': 'Optional text that appears within the attachment',
-                'fields': [
-                    {
-                        'title': 'Priority',
-                        'value': 'High',
-                        'short': False
-                    }
-                ],
-                'image_url': 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350',
-                'thumb_url': 'https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350'
-            }
-        ]
-    })
+    return "Well hey there!!!!"
